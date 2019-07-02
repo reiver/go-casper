64dab272f5d26842891db623639dadd5bfabcac3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-07-02 16:50:20 -0700

    casper.encodeRightCurlyBracket

---
+++ encode_rightcurlybracket_test.go
@@ -0,0 +1,26 @@
+package casper
+
+import (
+	"bytes"
+
+	"testing"
+)
+
+func TestEncodeRightCurlyBracket(t *testing.T) {
+
+	var buffer bytes.Buffer
+
+	if err := encodeRightCurlyBracket(&buffer); nil != err {
+		t.Errorf("Did not expect an error, but actually got one.")
+		t.Logf("ERROR type: %T", err)
+		t.Logf("ERRORe: %q", err)
+		return
+	}
+
+	if expected, actual := "}", buffer.String(); expected != actual {
+		t.Errorf("Expected a single right curly bracket to be written, but that is not what actually got.")
+		t.Logf("EXPECTED: %q", expected)
+		t.Logf("ACTUAL:   %q", actual)
+		return
+	}
+}
