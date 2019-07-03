248ceeb759860cc35f0c78d629a4b833ee3de1a3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-07-02 18:21:42 -0700

    casper.ShortWrite

--- shortwrite_test.go
+++ shortwrite_test.go
@@ -12,3 +12,12 @@ func TestInternalShortWriteAsError(t *testing.T) {
 		return
 	}
 }
+
+func TestInternalShortWriteAsShortWrite(t *testing.T) {
+	var err ShortWrite = internalShortWrite{} // THIS IS THE LINE THAT ACTUALLY MATTERS.
+
+	if nil == err {
+		t.Errorf("This should never happen.")
+		return
+	}
+}
